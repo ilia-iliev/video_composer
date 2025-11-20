8b18f0137c00c8575e5a6381a5fd717bddecd5a9
Ilia <ilia.iliev94@gmail.com>
2025-11-20 15:21:06 +0200

    renamed scripts

--- describe.sh
+++ describe.sh
@@ -2,7 +2,7 @@
 
 source config.env
 
-prompt_template=$(cat describe.txt)
+prompt_template=$(cat prompt_to_describe.txt)
 samples_dir="$VIDEO_HOME/$NEW_VIDEO_NAME"
 
 gemini --include-directories "$samples_dir" -p "$prompt_template" -m gemini-2.5-pro >> "$samples_dir/description.txt"
